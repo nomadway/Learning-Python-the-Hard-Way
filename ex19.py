50947aee2dcb98e7e31d9bb52f8f95702af107d3
nomadway <ubolxyz@gmail.com>
2021-03-02 23:07:59 +0600

    chapter19

---
+++ ex19.py
@@ -0,0 +1,33 @@
+
+# def cheese_and_crackers(cheese_count, boxes_of_crackers):
+# 	print(f"You have {cheese_count} cheeses!")
+# 	print(f"You have {boxes_of_crackers} boxes of crackers!")
+# 	print("Man, that's enough for a party.")
+# 	print("Get a blanket.\n")
+
+# print("We can just give the function numbers directy: ")
+# cheese_and_crackers(20, 30)
+
+# print("Or, we can use variables from our script: ")
+# amount_of_cheese = 100
+# amount_of_crackers = 200
+
+# print("We can even do Math inside too:")
+# cheese_and_crackers(1+1, 2+2)
+
+
+# print("And we can combine the two, variables and math: ")
+# cheese_and_crackers(amount_of_cheese+500, amount_of_crackers+1000)
+
+
+
+# cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+######################################
+
+def milk_and_honey(milk_count, honey_count):
+	print(f"I have {milk_count} litres of fresh milk.")
+	print(f"Mary has {honey_count} bottles of honey.")
+	print("Girl, that should do for the coming winter.")
+print("Function numbers '1' and '3' can be given directly. ")
+milk_and_honey(1, 3)	
